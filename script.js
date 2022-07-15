4577cbb6ce5ad4ff83ac579f8f6fb804b536bec3
John <johnw_pratt@outlook.com>
2022-07-15 14:25:12 -0500

    Boiler Plate Javascript File for Tic Tac Toe. Has the begging function for players and computer choices.  Also starts with a loop for a total of best out of 5 games.

---
+++ script.js
@@ -0,0 +1,15 @@
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+    function getComputerChoice() {
+        // your code here!
+    }
+
+    for (let i = 0; i < 5; i++) {
+        
+    }

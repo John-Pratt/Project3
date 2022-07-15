d47dcb987581f68d8e6e32657d2a18ebdcc5d439
John <johnw_pratt@outlook.com>
2022-07-15 15:23:39 -0500

    Added Username and Scores by getElementById.  Used a function for the username and submit button.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+let player1Score = 0;
+document.getElementById("p1").innerHTML = "Player 1: " + player1Score;
+
+let player2Score = 0;
+document.getElementById("p2").innerHTML = "Player 2: " + player2Score;
+
+
+document.getElementById("myButton").onclick = function() {
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
+
+
+
+
+
+/* Below is the starting code for the game.
 function playRound(playerSelection, computerSelection) {
     // your code here!
   }
@@ -11,5 +29,6 @@ function playRound(playerSelection, computerSelection) {
     }
 
     for (let i = 0; i < 5; i++) {
-        
+
     }
+*/

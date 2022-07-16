ab9dcae94ca78c834dc163dc3398be514fa1456f
John <johnw_pratt@outlook.com>
2022-07-16 14:37:06 -0500

    Added the functionality to enter Rock Paper or Scissors.  Used that functionality to build return statements based on what the user input.  Have not wrote functionality for the computer side yet.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let player1Score = 0;
 document.getElementById("p1").innerHTML = "Player 1: " + player1Score;
 
 let player2Score = 0;
-document.getElementById("p2").innerHTML = "Player 2: " + player2Score;
+document.getElementById("p2").innerHTML = "Computer: " + player2Score;
 
 
 document.getElementById("myButton").onclick = function() {
@@ -11,25 +11,36 @@ document.getElementById("myButton").onclick = function() {
     document.getElementById("p1").innerHTML = username + ": " + player1Score;
 }
 
+document.getElementById("playerSelection").onclick = function() {
+    playerSelection = document.getElementById("tttText").value;
+}
 
+function game() {
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return alert("It is a Tie!");
+        }
+        if (playerSelection === "rock") {
+            if (computerSelection === "paper") {
+                return alert("You lose. Paper beats Rock!");
+            }
+            else if (computerSelection === "scissors")
+                return alert("You Win! Rock beats Sciccors");
+      }
+    }
 
-
-
-
-/* Below is the starting code for the game.
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-   
+}
+/*
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
 
     function getComputerChoice() {
-        // your code here!
+        
     }
 
+
+    
     for (let i = 0; i < 5; i++) {
 
     }
-*/
